90960b405f244a225ec7862644f44a7a4298ae7d
Peter Byun <peter1035k@gmail.com>
2024-05-01 00:39:47 +0900

    feat: improve the snap points example

--- apps/html/src/features/snap-points.ts
+++ apps/html/src/features/snap-points.ts
@@ -8,19 +8,34 @@ let state = {
 export function setupSnapPoints() {
   const bottomSheet = createBottomSheet({
     content: `<div class="dragging-detection"> 
-            <h2>Fill the inputs!</h2> 
-            <input 
+            <h2>Focus on the inputs 🎯</h2> 
+            <div>
+              <label for="input-step-0">First Input</label>
+              <input 
                 id="input-step-0"
-            />
-            <input 
-                id="input-step-1"
-            />
-            <input 
-                id="input-step-2"
-            />
-            <input 
-                id="input-step-3"
-            />
+              />
+            </div>
+
+            <div>
+              <label for="input-step-1">Second Input</label>
+              <input 
+              id="input-step-1"
+              />
+            </div>
+
+            <div>
+              <label for="input-step-2">Third Input</label>
+              <input 
+                  id="input-step-2"
+              />
+            </div>
+
+            <div>
+              <label for="input-step-3">Last Input</label>
+              <input 
+              id="input-step-3"
+              />
+            </div>
         </div>`,
     snapPoints: state.snapPoints,
   });
@@ -36,6 +51,12 @@ export function setupSnapPoints() {
         bottomSheet.snapTo(state.snapPoints[state.step]);
       });
   });
+
+  document
+    .querySelector<HTMLInputElement>(`#input-step-3`)
+    ?.addEventListener("blur", () => {
+      bottomSheet.close();
+    });
 
   return {
     snapPointsBottomSheet: bottomSheet,
