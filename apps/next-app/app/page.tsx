5edbbce64d425df828795880b787e576852c7547
Peter Byun <peter1035k@gmail.com>
2024-05-02 00:04:54 +0900

    feat: test the published bottom-sheet

--- apps/next-app/app/page.tsx
+++ apps/next-app/app/page.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import { useEffect } from "react";
 import styles from "./page.module.css";
 
+import { createBottomSheet } from "plain-bottom-sheet-core";
+
 export default function Home() {
+  useEffect(() => {
+    const bottomSheet = createBottomSheet({
+      content: "Hi",
+    });
+
+    bottomSheet.mount();
+    bottomSheet.open();
+  }, []);
   return <main className={styles.main}></main>;
 }
