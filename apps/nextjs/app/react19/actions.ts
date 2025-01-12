a689377dcb30ef1797be15d042938ef3fd481f2b
peter-byun <peter1035k@gmail.com>
2025-01-13 00:59:19 +0900

    docs(nextjs): delete teh ramdom error

--- apps/nextjs/app/react19/actions.ts
+++ apps/nextjs/app/react19/actions.ts
@@ -3,11 +3,5 @@
 export async function submitNameFormData(formData: { name: string }) {
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  const succeeded = Math.random() > 0.5;
-
-  if (succeeded) {
-    return { message: formData.name };
-  } else {
-    return new Error("Failed to submit your name");
-  }
+  return { message: formData.name };
 }
