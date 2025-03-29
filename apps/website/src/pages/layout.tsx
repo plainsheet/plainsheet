f2c2c78da40d5937affa451a26c4d0391f2b0be0
peter-byun <peter1035k@gmail.com>
2025-03-29 12:44:53 +0900

    feat(website): collect vercel analytics for development decision making

---
+++ apps/website/src/pages/layout.tsx
@@ -0,0 +1,14 @@
+import { Analytics } from "@vercel/analytics/react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+      <Analytics />
+    </html>
+  );
+}
